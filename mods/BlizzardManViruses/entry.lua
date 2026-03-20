9b736b28786e1adaf4025bdde17b7fa5b58cae43
Arthur Cosentino <arthurcose@gmail.com>
2026-03-19 22:29:29 -0400

    Add Cactikil to BlizzardMan's encounters

--- mods/BlizzardManViruses/entry.lua
+++ mods/BlizzardManViruses/entry.lua
@@ -3,10 +3,11 @@ local SharedLib = require("dev.konstinople.LiberationServer.Shared")
 local LiberationLib = require("dev.konstinople.library.liberation")
 
 local VIRUS_POOL = {
-    { 2, "BattleNetwork3.enemy.Gloomer", Rank.V1 }, -- 140 hp, 60 damage
-    { 2, "BattleNetwork6.Mettaur.Enemy", Rank.V3 }, -- 120 hp, 50 damage, blue
-    { 1, "BattleNetwork6.Mettaur.Enemy", Rank.SP }, -- 160 hp, 70 damage, grey / blue symbols
-    { 1, "BattleNetwork6.Swordy.Enemy",  Rank.V3 }, -- 160 hp, 80 damage
+    { 2, "BattleNetwork3.enemy.Gloomer",  Rank.V1 }, -- 140 hp, 60 damage
+    { 2, "BattleNetwork6.Mettaur.Enemy",  Rank.V3 }, -- 120 hp, 50 damage, blue
+    { 1, "BattleNetwork6.Mettaur.Enemy",  Rank.SP }, -- 160 hp, 70 damage, grey / blue symbols
+    { 1, "BattleNetwork6.Swordy.Enemy",   Rank.V3 }, -- 160 hp, 80 damage
+    { 2, "BattleNetwork5.Cactroll.Enemy", Rank.EX }, -- 190 hp, 100 damage
 }
 
 ---@param encounter Encounter
