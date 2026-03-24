f466d7ef89f0312904dbc3d0c660b7dccca4c9ca
Arthur Cosentino <arthurcose@gmail.com>
2026-03-24 12:48:29 -0400

    Improve tutorial prog dialog

--- scripts/tutorial_prog.lua
+++ scripts/tutorial_prog.lua
@@ -22,23 +22,60 @@ local id = Net.create_bot({
   solid = true
 })
 
-local LIBERATIONS_HELP_TEXT = [[
-LIBERATION MISSIONS ARE A GAME MODE ADDED IN MMBN5.
-THE GOAL OF A LIBERATION MISSION IS TO DEFEAT THE BOSS AT THE END OF THE AREA.
-IN THE WAY OF YOUR PATH ARE PURPLE TILES CALLED DARK PANELS. TO LIBERATE A PANEL YOU MUST WIN A BATTLE WITHIN THREE TURNS.
-]]
+local LIBERATIONS_SCRIPT = {
+  "AREAS OF THE NET HAVE BEEN TAKEN OVER BY DARKLOIDS.",
+  "ENTERING THIS DOOR WILL BEGIN A MISSION TO LIBERATE AN AREA FROM THEIR CONTROL.",
+  "THERE ARE DARK PANELS IN YOUR PATH.",
+  "TO LIBERATE A PANEL YOU MUST WIN A BATTLE WITHIN THREE TURNS.",
+  "ONCE YOU'VE COMPLETED YOUR ACTION FOR THE TURN, A CHECKMARK WILL APPEAR OVER YOUR HEAD. LIKE THIS:",
+  function(player_id)
+    Net.exclusive_player_emote(player_id, player_id, Emotes.GREEN_CHECK)
+    Async.await(Async.sleep(2))
+  end,
+  "YOU WILL BE UNABLE TO MOVE UNTIL ALL PLAYERS HAVE COMPLETED THEIR TURN FOR THE PHASE.",
+  "PRESSING THE [CONFIRM] BUTTON WHILE IN THIS STATE WILL CYCLE BETWEEN PLAYERS TO FOLLOW.",
+  "WHEN ANY PLAYER STARTS A BATTLE YOU'LL JOIN IN AS A SPECTATOR."
+}
 
-local PARTIES_INTRO_TEXT = [[
-PARTIES ALLOW YOU TO PLAY LIBERATION MISSIONS IN A GROUP. TO START A PARTY, WALK UP TO ANOTHER PLAYER AND PRESS interact. YOU WILL SEE A MENU THAT LOOKS LIKE THIS:
-]]
+local PARTIES_SCRIPT = {
+  "PARTIES ALLOW YOU TO PLAY LIBERATION MISSIONS AS A TEAM.",
+  "TO START A PARTY, WALK UP TO ANOTHER PLAYER AND PRESS [CONFIRM].",
+  "YOU WILL SEE A MENU THAT LOOKS LIKE THIS:",
+  function(player_id)
+    local mugshot = Net.get_player_mugshot(player_id)
+    Net.question_player(player_id, "Recruit Anon?", mugshot.texture_path, mugshot.animation_path)
+  end,
+  "IF YOU SELECT YES, THE OTHER PLAYER WILL SEE A ? ABOVE YOUR HEAD. LIKE THIS:",
+  function(player_id)
+    Net.exclusive_player_emote(player_id, id, Emotes.QUESTION)
+    Async.await(Async.sleep(2))
+  end,
+  "WHEN YOU SEE A PLAYER WITH A ? ABOVE THEIR HEAD, INTERACTING WITH THAT PLAYER WILL ALLOW YOU TO RESPOND TO THEIR REQUEST.",
+  "IF YOU ACCEPT YOU WILL BE ADDED TO THEIR PARTY AND BOTH MEMBERS WILL BE NOTIFIED THROUGH THIS INDICATOR:",
+  function(player_id)
+    Net.exclusive_player_emote(player_id, id, Emotes.HAPPY)
+    Net.exclusive_player_emote(player_id, player_id, Emotes.HAPPY)
+    Async.await(Async.sleep(2))
+  end
+}
 
-local PARTIES_SIGNAL_TEXT = [[
-IF YOU SELECT YES, THE OTHER PLAYER WILL SEE A ? ABOVE YOUR HEAD. LIKE THIS:
-]]
+-- expects to run within an async scope
+local function process_tutorial_script(player_id, script)
+  for i = 1, #script do
+    local current = script[i]
+    local next = script[i + 1]
 
-local PARTIES_RESPONSE_TEXT = [[
-WHEN YOU SEE A PLAYER WITH A ? ABOVE THEIR HEAD, INTERACTING WITH THAT PLAYER WILL ALLOW YOU TO RESPOND TO THEIR REQUEST. IF YOU ACCEPT YOU WILL BE ADDED TO THEIR PARTY AND BOTH MEMBERS WILL BE NOTIFIED THROUGH THIS INDICATOR:
-]]
+    if type(current) == "function" then
+      current(player_id)
+    elseif type(next) == "function" then
+      -- need to wait
+      Async.await(Async.message_player(player_id, current, prog_textbox_options))
+    else
+      -- don't need to wait
+      Net.message_player(player_id, current, prog_textbox_options)
+    end
+  end
+end
 
 local receiving_help = {}
 
@@ -67,25 +104,10 @@ local help = Async.create_function(function(player_id)
 
     if response == 0 then
       -- liberations
-      Net.message_player(player_id, LIBERATIONS_HELP_TEXT, prog_textbox_options)
+      process_tutorial_script(player_id, LIBERATIONS_SCRIPT)
     elseif response == 1 then
       -- parties
-      local mugshot = Net.get_player_mugshot(player_id)
-
-      -- initial explanation
-      Net.message_player(player_id, PARTIES_INTRO_TEXT, prog_textbox_options)
-      Net.question_player(player_id, "Recruit Anon?", mugshot.texture_path, mugshot.animation_path) -- recruit example
-
-      -- show how request signaling works
-      Async.await(Async.message_player(player_id, PARTIES_SIGNAL_TEXT, prog_textbox_options))
-      Net.exclusive_player_emote(player_id, id, Emotes.QUESTION)
-      Async.await(Async.sleep(2))
-
-      -- show what an accepted request looks like
-      Async.await(Async.message_player(player_id, PARTIES_RESPONSE_TEXT, prog_textbox_options))
-      Net.exclusive_player_emote(player_id, id, Emotes.HAPPY)
-      Net.exclusive_player_emote(player_id, player_id, Emotes.HAPPY)
-      Async.await(Async.sleep(2))
+      process_tutorial_script(player_id, PARTIES_SCRIPT)
     elseif response == 2 then
       break
     end
