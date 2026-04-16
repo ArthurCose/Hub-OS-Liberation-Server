de2f2416c1b2ec2532e4e215ff45ab9c4176a6c2
Arthur Cosentino <arthurcose@gmail.com>
2026-04-16 03:08:14 -0400

    Prepare NebulaArea3

---
+++ assets/tiles/nebula3_unliberated.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="nebula3_unliberated" tilewidth="1153" tileheight="624" tilecount="1" columns="1" objectalignment="topleft">
+ <tileoffset x="1" y="0"/>
+ <image source="nebula3_unliberated.png" width="1153" height="624"/>
+</tileset>
