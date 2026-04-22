3d8ebd1b7b4e7c27b66c9ef560ca3da18a930070
Arthur Cosentino <arthurcose@gmail.com>
2026-04-21 20:36:12 -0400

    Fix shademan virus ranks and dependencies

--- mods/ShadeManViruses/entry.lua
+++ mods/ShadeManViruses/entry.lua
@@ -4,8 +4,8 @@ local LiberationLib = require("dev.konstinople.library.liberation")
 
 local VIRUS_POOL = {
     { 2, "BattleNetwork6.Mettaur.Enemy",  Rank.Rare1 }, -- 120 hp, 50 damage, cracks panels, purple
-    { 1, "BattleNetwork5.Gnarly.Enemy",   Rank.V3 },    -- 200 hp, 160 damage
-    { 1, "BattleNetwork5.Gnarly.Enemy",   Rank.V3 },    -- duplicated for higher selection chance
+    { 1, "BattleNetwork5.Gnarly.Enemy",   Rank.V1 },    -- 200 hp, 160 damage
+    { 1, "BattleNetwork5.Gnarly.Enemy",   Rank.V1 },    -- duplicated for higher selection chance
     { 1, "BattleNetwork5.Gnarly.Enemy",   Rank.EX },    -- 240 hp, 200 damage
     { 1, "BattleNetwork5.Cacter.Enemy",   Rank.V1 },    -- 230 hp, 150 damage
     { 1, "BattleNetwork5.Bugtank3.Enemy", Rank.V1 },    -- 240 hp, 200 damage
