7e9f65806f1aec23901e37cfb6b6ccf9671a963d
Arthur Cosentino <arthurcose@gmail.com>
2026-04-24 07:19:32 -0400

    Fix Nebula 3 layering

--- assets/tiles/nebula3_unliberated.tsx
+++ assets/tiles/nebula3_unliberated.tsx
@@ -1,5 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="nebula3_unliberated" tilewidth="1153" tileheight="624" tilecount="1" columns="1" objectalignment="topleft">
  <tileoffset x="1" y="0"/>
+ <properties>
+  <property name="Layer" type="int" value="15"/>
+ </properties>
  <image source="nebula3_unliberated.png" width="1153" height="624"/>
 </tileset>
