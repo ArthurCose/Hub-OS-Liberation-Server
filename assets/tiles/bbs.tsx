f00388d2875c3c3463ee7adbf551cb81345cf4c8
Arthur Cosentino <arthurcose@gmail.com>
2026-05-13 22:53:32 -0400

    Update BBS sprites and position

--- assets/tiles/bbs.tsx
+++ assets/tiles/bbs.tsx
@@ -1,13 +1,34 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="bbs" tilewidth="32" tileheight="56" tilecount="1" columns="1" objectalignment="bottom">
+<tileset version="1.10" tiledversion="1.12.1" name="bbs" tilewidth="32" tileheight="56" tilecount="4" columns="2" objectalignment="bottom">
  <tileoffset x="0" y="16"/>
  <grid orientation="isometric" width="64" height="32"/>
  <transformations hflip="0" vflip="1" rotate="0" preferuntransformed="0"/>
- <image source="bbs.png" width="32" height="56"/>
+ <image source="bbs.png" width="64" height="112"/>
  <tile id="0">
   <objectgroup draworder="index" id="5">
-   <object id="18" x="16" y="48.5333">
-    <polygon points="1.5,-0.0333 1.5,-32.0333 -25,-57.5333 -31,-23.5333 -3,4.4667"/>
+   <object id="19" x="16" y="48.5333">
+    <polygon points="1.5,-0.0333 1.5,-32.0333 -16,-48.5333 -16.5696,-9.15621 -3,4.4667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="16" y="48.5333">
+    <polygon points="1.5,-0.0333 1.5,-32.0333 -16,-48.5333 -16.5696,-9.15621 -3,4.4667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="16" y="48.5333">
+    <polygon points="1.5,-0.0333 1.5,-32.0333 -16,-48.5333 -16.5696,-9.15621 -3,4.4667"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="48.5333">
+    <polygon points="1.5,-0.0333 1.5,-32.0333 -16,-48.5333 -16.5696,-9.15621 -3,4.4667"/>
    </object>
   </objectgroup>
  </tile>
