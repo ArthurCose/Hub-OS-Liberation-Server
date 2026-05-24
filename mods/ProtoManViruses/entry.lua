4e4882ff0d643311b0b35eb742ece2481f0c0890
Arthur Cosentino <arthurcose@gmail.com>
2026-05-24 17:53:01 -0400

    Nerf undernet4 demon eye

--- mods/ProtoManViruses/entry.lua
+++ mods/ProtoManViruses/entry.lua
@@ -3,14 +3,14 @@ local SharedLib = require("dev.konstinople.LiberationServer.Shared")
 local LiberationLib = require("dev.konstinople.library.liberation")
 
 local VIRUS_POOL = {
-    { 1, "BattleNetwork5.Dominerd.Enemy",  Rank.V1 },    -- 160 hp, 60 damage
-    { 1, "BattleNetwork5.Dominerd.Enemy",  Rank.V1 },    -- duplicated for higher selection chance
-    { 2, "BattleNetwork5.CanRaid.Enemy",   Rank.EX },    -- 180 hp, 100 damage
-    -- { 1, "BattleNetwork6.Swordy.Enemy",   Rank.Rare1 }, -- 160 hp, 90 damage
-    { 2, "BattleNetwork6.Swordy.Enemy",    Rank.Rare2 }, -- 220 hp, 120 damage
-    { 2, "BattleNetwork6.BigHat.Enemy",    Rank.V2 },    -- 150 hp, 50 damage
-    { 1, "BattleNetwork6.BigHat.Enemy",    Rank.SP },    -- 250 hp, 100 damage
-    { 1, "BattleNetwork3.enemy.KillerEye", Rank.SP },    -- 230 hp, 200 damage
+    { 1, "BattleNetwork5.Dominerd.Enemy", Rank.V1 },    -- 160 hp, 60 damage
+    { 1, "BattleNetwork5.Dominerd.Enemy", Rank.V1 },    -- duplicated for higher selection chance
+    { 2, "BattleNetwork5.CanRaid.Enemy",  Rank.EX },    -- 180 hp, 100 damage
+    -- { 2, "BattleNetwork6.Swordy.Enemy",   Rank.V3 }, -- 160 hp, 90 damage
+    { 2, "BattleNetwork6.Swordy.Enemy",   Rank.Rare2 }, -- 220 hp, 120 damage
+    { 2, "BattleNetwork6.BigHat.Enemy",   Rank.V2 },    -- 150 hp, 50 damage
+    { 1, "BattleNetwork6.BigHat.Enemy",   Rank.SP },    -- 250 hp, 100 damage
+    { 1, "BattleNetwork3.enemy.DemonEye", Rank.V1 },    -- 150 hp, 100 damage
 }
 
 ---@param encounter Encounter
