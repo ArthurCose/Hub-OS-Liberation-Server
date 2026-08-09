801926892ee8e435894d852cc6165ae68f2ad4e2
Arthur Cosentino <arthurcose@gmail.com>
2026-08-09 12:03:46 -0400

    Switch BigHat SP for V1

--- mods/ProtoManViruses/entry.lua
+++ mods/ProtoManViruses/entry.lua
@@ -7,8 +7,9 @@ local VIRUS_POOL = {
     { 1, "BattleNetwork5.CanRaid.Enemy",  Rank.EX },    -- 180 hp, 100 damage
     -- { 2, "BattleNetwork6.Swordy.Enemy",   Rank.V3 }, -- 160 hp, 90 damage
     { 2, "BattleNetwork6.Swordy.Enemy",   Rank.Rare2 }, -- 220 hp, 120 damage
+    { 1, "BattleNetwork6.BigHat.Enemy",   Rank.V1 },    -- 100 hp, 30 damage
     { 2, "BattleNetwork6.BigHat.Enemy",   Rank.V2 },    -- 150 hp, 50 damage
-    { 1, "BattleNetwork6.BigHat.Enemy",   Rank.SP },    -- 250 hp, 100 damage
+    -- { 1, "BattleNetwork6.BigHat.Enemy",   Rank.V1 },    -- 250 hp, 100 damage
     { 1, "BattleNetwork3.enemy.DemonEye", Rank.V1 },    -- 150 hp, 100 damage
 }
 
