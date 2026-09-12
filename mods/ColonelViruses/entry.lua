c0b382b0a94e20ba3ce12fd571a7061dfcd13843
Arthur Cosentino <arthurcose@gmail.com>
2026-09-12 08:42:12 -0400

    Buff DarkMechs in Colonel's area

--- mods/ColonelViruses/entry.lua
+++ mods/ColonelViruses/entry.lua
@@ -5,7 +5,7 @@ local VIRUS_POOL = {
     { 1, "BattleNetwork6.Gunner.Enemy",   Rank.V2 }, -- 140 hp, 30 damage
     { 2, "BattleNetwork6.Gunner.Enemy",   Rank.V3 }, -- 220 hp, 50 damage
     { 2, "BattleNetwork.Enemy.Catack",    Rank.V2 }, -- 160 hp, 100 damage
-    { 1, "BattleNetwork6.DarkMech.Enemy", Rank.V1 }, -- 180 hp, 100 damager
+    { 1, "BattleNetwork6.DarkMech.Enemy", Rank.V3 }, -- 270 hp, 150 damage
     { 2, "BattleNetwork5.Gnarly.Enemy",   Rank.V1 }, -- 200 hp, 160 damage
 }
 
